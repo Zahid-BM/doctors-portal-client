08b574869e59b91f7db07c5fa7be1d305576f063
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-19 20:29:23 +0600

    Alhamdulillah navbar setups with links and routes setups. Working fine

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import About from './pages/About/About';
+import Appointment from './pages/Appointment/Appointment';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import Reviews from './pages/Reviews/Reviews';
 import Navbar from './pages/shared/Navbar';
 
 function App() {
@@ -11,6 +14,9 @@ function App() {
       <Routes>
         <Route path='/' element={<Home />}></Route>
         <Route path='/about' element={<About />}></Route>
+        <Route path='/appointment' element={<Appointment />}></Route>
+        <Route path='/reviews' element={<Reviews />}></Route>
+        <Route path='/login' element={<Login />}></Route>
       </Routes>
     </div>
   );
