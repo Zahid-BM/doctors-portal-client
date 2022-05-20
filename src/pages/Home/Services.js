e1efbfc724522fdb6a3e0c263b620ce099ecb705
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-20 15:45:53 +0600

    Alhamdulillah our services part done

---
+++ src/pages/Home/Services.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import fluoride from '../../assets/images/fluoride.png';
+import cavity from '../../assets/images/cavity.png';
+import whitening from '../../assets/images/whitening.png';
+import SingleService from './SingleService';
+
+const Services = () => {
+    const services = [
+        {
+            _id: 1,
+            name: 'Flouride Treatment',
+            description: 'This is a very good treatment without any harm. Get treated and fell well.',
+            img: fluoride
+        },
+        {
+            _id: 1,
+            name: 'Cavity Filling',
+            description: 'This is a very good treatment without any harm. Get treated and fell well.',
+            img: cavity
+        },
+        {
+            _id: 1,
+            name: 'Teeth Whitening',
+            description: 'This is a very good treatment without any harm. Get treated and fell well.',
+            img: whitening
+        }
+    ];
+    return (
+        <div className='my-28'>
+            <div className='text-center'>
+                <h1 className='font-bold uppercase text-secondary text-2xl'>Our Services</h1>
+                <h1 className='text-4xl'>Services We Provide</h1>
+            </div>
+            <div className='grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 my-10 px-12'>
+                {
+                    services.map(service => <SingleService key={service._id} service={service}></SingleService>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default Services;
