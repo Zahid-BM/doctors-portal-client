9ba1f39772f7272eed3c4bc80258e96c18242ec9
Zahid-BM <bmzahidulislam@outlook.com>
2022-05-24 16:58:36 +0600

    Alhamdulillah login page creation and google login success

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
+
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    if (user) {
+        console.log(user)
+    }
     return (
-        <div>
-            <h1>This is Login Page</h1>
+        <div className='flex h-screen justify-center items-center'>
+            <div className='card w-96 shadow-2xl '>
+                <h1 className='text-center pt-8 font-bold text-2xl' >User Login</h1>
+                <div class="card-body">
+
+                    <div class="divider">OR</div>
+                    <button onClick={() => signInWithGoogle()} class="btn btn-outline">Continue with Google</button>
+                    <button class="btn btn-outline">Continue with GitHub</button>
+
+                </div>
+            </div>
+
         </div>
     );
 };
